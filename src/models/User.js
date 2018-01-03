5ea93842245f78d7ea94d509fec08ec2a258d555
Max <max_spree@hotmail.com>
2018-01-03 16:48:51 +0100

    Adapting server to Goodreads API & adding major changes

--- src/models/User.js
+++ src/models/User.js
@@ -6,9 +6,16 @@ import uniqueValidator from 'mongoose-unique-validator';
 // TODO: add uniqueness and email validations to email field
 const schema = new mongoose.Schema(
     {
-        email: { type: String, required: true, lowercase: true, index: true, unique: true },
+        email: {
+            type: String,
+            required: true,
+            lowercase: true,
+            index: true,
+            unique: true 
+        },
         passwordHash: { type: String, required: true },
-        confirmed: { type: Boolean, default: false }   
+        confirmed: { type: Boolean, default: false },
+        confirmationToken: { type: String, default: "" }   
     },
     { timestamps: true }
 );
@@ -17,17 +24,42 @@ schema.methods.isValidPassword = function isValidPassword(password) {
     return bcrpyt.compareSync(password, this.passwordHash);
 };
 
-schema.method.setPassword = function setPassword(password) {
+schema.methods.setPassword = function setPassword(password) {
     this.passwordHash = bcrpyt.hashSync(password, 10);
 };
 
-schema.methods.generateJWT = function generateJWT() {
-    return jwt.sign({
-        email: this.email
-    },
-    process.env.JWT_SECRET
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+    this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationUrl = function generateConfirmationUrl() {
+    return `${process.env.HOST}/confirmation/${this.confirmationToken}`;    
+};
+
+schema.methods.generateResetPasswordLink = function generateResetPasswordLink() {
+    return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`
+}
+
+schema.methods.generateResetPasswordToken = function generateResetPasswordToken() {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' }
     );
 };
+
+schema.methods.generateJWT = function generateJWT() {
+    return jwt.sign(
+        {
+            email: this.email,
+            confirmed: this.confirmed
+        },
+        process.env.JWT_SECRET
+    );
+};
+
 
 schema.methods.toAuthJSON = function toAuthJSON() {
     return {
@@ -35,7 +67,7 @@ schema.methods.toAuthJSON = function toAuthJSON() {
         confirmed: this.confirmed,
         token: this.generateJWT()
     };
-}
+};
 
 schema.plugin(uniqueValidator, { message: 'This email is already taken' });
 
